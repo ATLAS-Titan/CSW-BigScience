f15ece83c285e05fba618b4486bcb327148845ca
Sean Wilkinson <wilkinsonsr@ornl.gov>
2019-09-22 12:54:21 -0400

    Addressed comments for Sections 4.1 and 4.2

--- rescheduling.py
+++ rescheduling.py
@@ -4,9 +4,14 @@ job_traces = original_traces.sort_by_start_time()
 max_nodes = 18688
 
 for job in job_traces:
-    while count_active_nodes() + job["requested_nodes"] > max_nodes:
-        current_time = get_next_completion_time() # from active_nodes
+    active_nodes = count_active_nodes()
+    requested_nodes = job["requested_nodes"]
+    while active_nodes + requested_nodes > max_nodes:
+      # Advance the simulation time to the time that
+      # the next job ends, when resources will be freed.
+        current_time = get_next_completion_time()
         evict_and_log_completed_jobs()
+        active_nodes = count_active_nodes()
     active_jobs.append(job)
 
 while len(active_jobs) > 0:
